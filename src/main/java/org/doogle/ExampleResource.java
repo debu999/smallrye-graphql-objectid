1066658c939c0ea33e4ebf506a97f15f57922305
Debabrata Patnaik <debabrata_patnaik@live.com>
2024-01-14 15:03:53 +0530

    Test with custom serialization/deserialization config in smallrye-graphql
    # Usage for custom changes will be exclusive to smallrye-graphql viz. no ObjectID dependency.
    JsonBCreator.setJsonB(Collections.singletonList(OBJECTID_SERIALIZER),
            Collections.singletonList(OBJECTID_DESERIALIZER));

--- src/main/java/org/doogle/ExampleResource.java
+++ src/main/java/org/doogle/ExampleResource.java
@@ -1,16 +1,17 @@
 package org.doogle;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 
 @Path("/hello")
 public class ExampleResource {
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from RESTEasy Reactive";
-    }
+  @GET
+  @Produces(MediaType.TEXT_PLAIN)
+  public String hello() {
+    return "Hello from RESTEasy Reactive";
+  }
+
 }
--- src/main/java/org/doogle/graphql/ObjectIdResource.java
+++ src/main/java/org/doogle/graphql/ObjectIdResource.java
@@ -1,31 +1,70 @@
 package org.doogle.graphql;
 
+import io.smallrye.graphql.json.JsonBCreator;
 import io.smallrye.mutiny.Uni;
+import jakarta.json.JsonValue.ValueType;
+import jakarta.json.bind.serializer.DeserializationContext;
+import jakarta.json.bind.serializer.JsonbDeserializer;
+import jakarta.json.bind.serializer.JsonbSerializer;
+import jakarta.json.bind.serializer.SerializationContext;
+import jakarta.json.stream.JsonGenerator;
+import jakarta.json.stream.JsonParser;
+import java.lang.reflect.Type;
+import java.util.Collections;
+import java.util.List;
+import org.bson.types.ObjectId;
 import org.doogle.graphql.entity.ReactivePerson;
 import org.doogle.graphql.enums.Action;
-import org.eclipse.microprofile.graphql.*;
-
-import java.util.List;
+import org.eclipse.microprofile.graphql.Description;
+import org.eclipse.microprofile.graphql.GraphQLApi;
+import org.eclipse.microprofile.graphql.Mutation;
+import org.eclipse.microprofile.graphql.Name;
+import org.eclipse.microprofile.graphql.NonNull;
+import org.eclipse.microprofile.graphql.Query;
 
 @GraphQLApi
 public class ObjectIdResource {
 
-    @Query("Person")
-    public Uni<List<ReactivePerson>> fetchAllPersons()
-    {
-        return ReactivePerson.listAll();
+  public static JsonbSerializer<ObjectId> OBJECTID_SERIALIZER = new JsonbSerializer<>() {
+    @Override
+    public void serialize(ObjectId objectId, JsonGenerator jsonGenerator,
+        SerializationContext serializationContext) {
+      jsonGenerator.write(objectId.toString());
     }
+  };
 
-    @Mutation("Person")
-    public Uni<ReactivePerson> mutatePerson(@Name("person") @Description("CREATE/UPDATE/DELETE person")
-                                            ReactivePerson person,
-                                            @Name("action") @Description("action for mutation")
-                                            @NonNull Action action)
-    {
-        if (action==Action.DELETE)
-        {
-            return ReactivePerson.deleteByIdentifier(person);
+  public static JsonbDeserializer<ObjectId> OBJECTID_DESERIALIZER = new JsonbDeserializer<>() {
+    @Override
+    public ObjectId deserialize(JsonParser jsonParser,
+        DeserializationContext deserializationContext, Type type) {
+      try {
+        if (jsonParser.getValue().getValueType() == ValueType.NULL) {
+          return null;
+        } else {
+          return new ObjectId(jsonParser.getString());
         }
-        return ReactivePerson.persistOrUpdate(person).map(ignored -> person);
+      } catch (Exception e) {
+        throw new RuntimeException(e);
+      }
     }
+  };
+
+  @Query("Person")
+  public Uni<List<ReactivePerson>> fetchAllPersons() {
+
+    JsonBCreator.setJsonB(Collections.singletonList(OBJECTID_SERIALIZER),
+        Collections.singletonList(OBJECTID_DESERIALIZER));
+    return ReactivePerson.listAll();
+  }
+
+  @Mutation("Person")
+  public Uni<ReactivePerson> mutatePerson(
+      @Name("person") @Description("CREATE/UPDATE/DELETE person") ReactivePerson person,
+      @Name("action") @Description("action for mutation") @NonNull Action action) {
+    if (action == Action.DELETE) {
+      return ReactivePerson.deleteByIdentifier(person);
+    }
+    return ReactivePerson.persistOrUpdate(person).map(ignored -> person);
+  }
+
 }
